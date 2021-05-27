65a155f188296df76625329de9cab8c27a8bf908
jinx909 <it.matt.smith@gmail.com>
2021-05-27 15:05:35 -0400

    Update PS7zipUtility.psm1

--- PS7zipUtility/PS7zipUtility.psm1
+++ PS7zipUtility/PS7zipUtility.psm1
@@ -16,7 +16,7 @@
 .OUTPUTS 
     <N/A>
 .NOTES
-  Version:        1.0
+  Version:        0.0.5
   Author:         Matthew Smith
   Creation Date:  05/25/2021
   Purpose/Change: Interview pre-requisite for QuestionMark
